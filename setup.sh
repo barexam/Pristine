9efa6c9eecd90c7d7743acc50a13b98dfe2ffeb3
Daniel Miessler <daniel@danielmiessler.com>
2016-09-28 12:06:43 -0700

    Added contents.

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install zsh
+
+apt install zsh
+
+# Copy your zprezto directory to home
+
+cp -a ./zprezto ../.zprezto
+
+# Copy vimrc to home
+
+cp ./vimrc ../.vimrc
+
+# Copy zshrc to home
+
+cp ./zshrc ../.zshrc
+
+# Configure options
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+done
+
+# Change shell to zsh permanently
+
+chsh -s /bin/zsh
+
+# Message
+
+echo ""
+echo "Ok, now exit this shell and create a new one, and you should be all set."
+
+## PERFORMING UPDATES
+# Run this command from within .zprezto
+# git pull && git submodule update --init --recursive
+
